4f6a6f45c5bb10b8513e79af11f48b2458ba7525
Leo <leo.e.gatille@gmail.com>
2026-01-14 21:31:58 +0100

    connect with LM studio backend

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { createAgent, tool } from "langchain";
 import { ChatOpenAI } from "@langchain/openai";
-
 import * as z from "zod";
 
 const getWeather = tool((input) => `It's always sunny in ${input.city}!`, {
@@ -18,15 +17,11 @@ const llm = new ChatOpenAI({
  maxTokens: undefined,
  timeout: undefined,
  maxRetries: 2,
+ apiKey: "local_env",
  configuration: {
-  baseURL: "http://127.0.0.1:1234",
+  baseURL: "http://127.0.0.1:1234/v1",
  },
 });
-
-// const agent = createAgent({
-//  model: "claude-sonnet-4-5-20250929",
-//  tools: [getWeather],
-// });
 
 async function callMyBot() {
  const res = await llm.invoke("Comment tu vas ?");
